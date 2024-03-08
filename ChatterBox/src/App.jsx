ef5fcbc9eac14775132f729d87c1ca1aa639d3d2
SanyamSamS <sanyam.edu@gmail.com>
2024-03-07 20:27:28 -0500

    Created setup, using bootstrap, mongo, mongoose, and express sessions

--- ChatterBox/src/App.jsx
+++ ChatterBox/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +16,7 @@ function App() {
         <div className="col">
           Test Column 2
         </div>
+        <Outlet />
       </div>
     </div>
     </>
