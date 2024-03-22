307ccaa446ded89022ece601019619b843651355
SanyamSamS <sanyam.edu@gmail.com>
2024-03-21 21:45:28 -0400

    deployment test 3

--- server/server.js
+++ server/server.js
@@ -24,22 +24,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({origin: true}));
 
+app.use('/api', routes);
+
+if (process.env.NODE_ENV === 'production') {
+    
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+} 
+
 
 // Once the database connection is open, start the server
 db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
     });
-    app.use('/api', routes);
-
-    if (process.env.NODE_ENV === 'production') {
-        
-        app.use(express.static(path.join(__dirname, '../client/dist')));
-        
-        app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-        });
-    } 
 });
 
 
